01f11fbbc88bc4af8836a10a79e871b0ffef7acb
OrellanaJheremiasT <d4rkr4in.ttv@gmail.com>
2025-11-19 23:51:39 -0500

    main

--- src/views/executor.java
+++ src/views/executor.java
@@ -1,16 +1,51 @@
+
 package views;
 import controller.petadministrator;
-import controller.report;
-import model.pets;
+import java.util.Scanner;
 
 public class executor {
-    public static void main(String[] args) {
-        petadministrator admin = new petadministrator(5);
-        admin.addPet(new pets("Firulais", "Dog", 3));
 
-        System.out.println(report.petreport(admin.getPetList()));
+    private final petadministrator administrador;
+    private final Scanner scanner;
 
-        
+    public executor() {
+        this.administrador = new petadministrator(5);
+        this.scanner = new Scanner(System.in);
     }
+
+    public void viewmenu (){
+        int option;
+        do {
+            System.out.println("\n========== Pets Menu ==========");
+            System.out.println("1. Add Pet");
+            System.out.println("2. Print Pet Report");
+            System.out.println("0. Exit");
+            System.out.println("======================================");
+            System.out.print("Select an option: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("\n--- Pet Report ---");
+                    break;
+                case 0:
+                    System.out.println("Exiting the system...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+
+
+
+
+        } while (option != 0);
+    }
+    
+
+
 
 }
